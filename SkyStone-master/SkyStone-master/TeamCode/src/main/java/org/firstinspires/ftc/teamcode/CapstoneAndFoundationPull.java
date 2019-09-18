0925bd5cb3d4b3be91d3c24bcf313e1d42eda9cf
BlueBoat74 <Animated27engineer@gmail.com>
2019-09-17 19:35:34 -0500

    Update CapstoneAndFoundationPull.java

--- SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CapstoneAndFoundationPull.java
+++ SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CapstoneAndFoundationPull.java
@@ -3,30 +3,40 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp (name = "TeleopServoTest")
 public class CapstoneAndFoundationPull extends OpMode
 {
     Servo pullerServo = null;
 
-    double basePower = 0;
-    double fullExtend = 1;
+    ElapsedTime waitTime = new ElapsedTime();
+
+    double basePower = 1;
+    double fullExtend = 0;
     double currentPower = 0;
+
+    boolean speedReady;
 
     @Override
     public void init()
     {
         currentPower = basePower;
         pullerServo = hardwareMap.servo.get("Puller_Servo");
+        pullerServo.setPosition(0);
     }
 
     @Override
     public void loop()
     {
-        if (gamepad1.y)
+        speedReady = waitTime.milliseconds() > 500;
+
+        if (gamepad1.y && speedReady)
         {
             if (currentPower == basePower){ currentPower = fullExtend; pullerServo.setPosition(currentPower); }
             else if (currentPower == fullExtend) { currentPower = basePower; pullerServo.setPosition((currentPower));}
+
+            waitTime.reset();
         }
     }
 }
