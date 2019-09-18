2218696ee6d492f606331f18d8aaef2f83bd304f
Gregory <gregory.dina@outlook.com>
2019-09-17 20:27:54 -0500

    Update Teleop.java

--- SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -3,18 +3,27 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp (name = "Teleop")
 public class Teleop extends OpMode {
 
+    Servo pullerServo = null;
+
     Grabber grabber1 = new Grabber(hardwareMap);
-    ElapsedTime waitTime = new ElapsedTime();
+    public ElapsedTime waitTime = new ElapsedTime();
+
+    double basePower = 1;
+    double fullExtend = 0;
+    double currentPower = 0;
 
     boolean speedReady;
 
     @Override
     public void init() {
+
+        currentPower = basePower;
 
     }
 
@@ -22,6 +31,14 @@ public class Teleop extends OpMode {
     public void loop() {
 
         speedReady = waitTime.milliseconds() > 500;
+
+        if (gamepad1.x && speedReady){
+
+            if (currentPower == basePower){ currentPower = fullExtend; grabber1.Down(); }
+            else if (currentPower == fullExtend) { currentPower = basePower; grabber1.Up();}
+
+            waitTime.reset();
+        }
 
     }
 }
