4da705865278a0165ad12840ace859e4cddeba6c
BlueBoat74 <Animated27engineer@gmail.com>
2019-09-17 20:26:31 -0500

    Update Grabber.java

--- SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Grabber.java
+++ SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Grabber.java
@@ -7,6 +7,9 @@ public class Grabber
 {
     private Servo pullerServo = null;
 
+    private double basePower = 1;
+    private double fullExtend = 0;
+
     public Grabber(HardwareMap hardwareMap)
     {
         pullerServo = hardwareMap.servo.get("Puller_Servo");
@@ -16,11 +19,11 @@ public class Grabber
 
     public void Down()
     {
-        pullerServo.setPosition(0);
+        pullerServo.setPosition(fullExtend);
     }
 
     public void Up()
     {
-        pullerServo.setPosition(1);
+        pullerServo.setPosition(basePower);
     }
 }
