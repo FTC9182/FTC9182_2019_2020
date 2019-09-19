2add844db4c73a25c2e032e254470e725867d8c0
BlueBoat74 <Animated27engineer@gmail.com>
2019-09-18 19:47:00 -0500

    Update DriveTrain.java

--- SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
+++ SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTrain.java
@@ -1,6 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class DriveTrain
 {
+    public DriveTrain(HardwareMap hardwareMap)
+    {
 
+    }
 }
