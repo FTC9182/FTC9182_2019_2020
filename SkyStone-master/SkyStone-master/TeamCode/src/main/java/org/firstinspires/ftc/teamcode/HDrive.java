652103c2acfece8cb86410fdca37b0c2a66d735e
BlueBoat74 <Animated27engineer@gmail.com>
2019-09-22 13:50:01 -0500

    Hdrive

--- SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HDrive.java
+++ SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HDrive.java
@@ -1,9 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
-public class HDrive
-{
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class HDrive {
+
+    DcMotor ForwardRight = null;
+    DcMotor ForwardLeft = null;
+    DcMotor BackwardsRight = null;
+    DcMotor BackwardsLeft = null;
+    DcMotor Middle = null;
+
+    public HDrive(HardwareMap hardwareMap){
+        ForwardRight = hardwareMap.dcMotor.get("front_right");
+        ForwardLeft = hardwareMap.dcMotor.get("front_left");
+        BackwardsRight = hardwareMap.dcMotor.get("back_right");
+        BackwardsLeft = hardwareMap.dcMotor.get("back_left");
+        Middle = hardwareMap.dcMotor.get("middle");
 
 
+    }
 
 
 }
--- SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.teamcode.FirstRobotCode.DriveTrain;
+import org.firstinspires.ftc.teamcode.FirstRobotCode.Grabber;
 
 @TeleOp (name = "Teleop")
 public class Teleop extends OpMode {
