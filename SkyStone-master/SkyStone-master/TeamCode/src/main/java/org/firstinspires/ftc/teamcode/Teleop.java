c9072eaf6ab12d41a86bf2a52c3fc8d6001a2899
Gregory <gregory.dina@outlook.com>
2019-09-22 13:52:25 -0500

    Update Teleop.java

--- SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -1,6 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp (name = "Teleop")
 public class Teleop extends OpMode {
