71e02d6b103b358d91dbd27481d3b4007f76ef2e
BlueBoat74 <Animated27engineer@gmail.com>
2019-09-22 13:51:30 -0500

    ok

--- SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstRobotCode/Teleop.java
+++ SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FirstRobotCode/Teleop.java
@@ -4,7 +4,6 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.teamcode.FirstRobotCode.DriveTrain;
 import org.firstinspires.ftc.teamcode.Grabber;
 
 @TeleOp (name = "Teleop")
