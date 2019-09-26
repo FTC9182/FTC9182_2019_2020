a22dbee3402fc0fecd684f43cdab308b9ab787d9
Gregory <gregory.dina@outlook.com>
2019-09-25 20:02:21 -0500

    Create AutonomousFoundationSide.java

---
+++ SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousFoundationSide.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class AutonomousFoundationSide extends LinearOpMode {
+
+    Grabber grabber1 = null;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+
+        waitForStart();
+
+
+    }
+}
