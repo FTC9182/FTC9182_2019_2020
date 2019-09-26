a42b498c6491e26bdace1ee605676dd8ab4840ba
BlueBoat74 <Animated27engineer@gmail.com>
2019-09-25 20:02:28 -0500

    Update Teleop.java

--- SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
+++ SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -2,25 +2,45 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp (name = "Teleop")
 public class Teleop extends OpMode {
 
     HDrive hDrive = null;
     Grabber grabber = null;
+    public ElapsedTime waitTime = new ElapsedTime();
 
+    //Drive var
     double driveX;
     double driveY;
     double turnDegrees;
+
+    //Grabber var
+    double currentPower;
+    double basePower = 1;
+    double fullExtend = 0;
+
+    boolean speedReady;
 
     public void init(){
 
         hDrive = new HDrive(hardwareMap);
         grabber = new Grabber(hardwareMap);
-
+        grabber.Up();
+        currentPower = basePower;
     }
 
     public void loop(){
+
+        speedReady = waitTime.milliseconds() > 500;
+
+        if (gamepad1.x && speedReady){
+
+            if (currentPower == basePower){ currentPower = fullExtend; grabber.Down(); }
+            else if (currentPower == fullExtend) { currentPower = basePower; grabber.Up();}
+
+            waitTime.reset();
 
         driveX = gamepad1.left_stick_x;
         driveY = gamepad1.left_stick_y;
