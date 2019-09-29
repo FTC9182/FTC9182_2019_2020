3512d6663f68c1e5bebe3918ae7891e1fc308229
Gregory <gregory.dina@outlook.com>
2019-09-29 15:46:24 -0500

    rip

--- SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
+++ SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
@@ -1,7 +1,9 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+@TeleOp (name = "EncoderTest")
 public class EncoderTest extends OpMode {
 
     HDrive hDrive = null;
@@ -14,6 +16,16 @@ public class EncoderTest extends OpMode {
 
     @Override
     public void loop() {
+
+        if (gamepad1.a){
+            hDrive.ForwardRight.setPower(1);
+        } else if (gamepad1.b){
+            hDrive.ForwardRight.setPower(-1);
+        } else {
+            hDrive.ForwardRight.setPower(0);
+        }
+
+        telemetry.addData("ForwardRight",hDrive.ForwardRight.getCurrentPosition());
 
     }
 }
