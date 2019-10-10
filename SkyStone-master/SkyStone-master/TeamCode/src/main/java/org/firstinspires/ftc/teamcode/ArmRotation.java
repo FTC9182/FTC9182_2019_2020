9853ecd8b73e2178441b2e1530f91ca90f83984f
BlueBoat74 <Animated27engineer@gmail.com>
2019-10-09 20:12:36 -0500

    Update ArmRotation.java

--- SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmRotation.java
+++ SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmRotation.java
@@ -1,4 +1,35 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
 public class ArmRotation {
+
+    DcMotor armRotation = null;
+    private double power = .2;
+    private int currentPosition;
+    private int rotationIndex = 5;
+    private ElapsedTime armTimer = new ElapsedTime();
+
+    public ArmRotation(HardwareMap hardwareMap){
+        armRotation.setPower(power);
+        armRotation.setTargetPosition(260);
+        armRotation.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armRotation = hardwareMap.dcMotor.get("arm_rotate");
+    }
+
+    public void Up(){
+        armRotation.setTargetPosition(currentPosition + rotationIndex);
+        armRotation.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armRotation.setPower(power);
+        currentPosition = armRotation.getCurrentPosition();
+    }
+
+    public void Down(){
+        armRotation.setTargetPosition(currentPosition - rotationIndex);
+        armRotation.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        armRotation.setPower(power);
+        currentPosition = armRotation.getCurrentPosition();
+    }
 }
