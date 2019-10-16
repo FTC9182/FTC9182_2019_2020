485e98ee9092a83896d1f77e7d367cdad16e94ed
BlueBoat74 <Animated27engineer@gmail.com>
2019-10-15 20:10:12 -0500

    Update ArmRotation.java

--- SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmRotation.java
+++ SkyStone-master/SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ArmRotation.java
@@ -7,15 +7,15 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 public class ArmRotation {
 
     DcMotor armRotation = null;
-    private double power = .5;
+    private double power = 1;
     private int currentPosition;
     private int rotationIndex = 15;
 
     public ArmRotation(HardwareMap hardwareMap){
         armRotation = hardwareMap.dcMotor.get("arm_rotate");
         armRotation.setPower(power);
-        armRotation.setTargetPosition(60);
-        armRotation.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        //armRotation.setTargetPosition(60);
+        //armRotation.setMode(DcMotor.RunMode.RUN_TO_POSITION);
         armRotation.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
     }
 
